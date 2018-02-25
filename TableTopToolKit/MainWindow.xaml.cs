cb5dc294d99b4c23b2cdd846f2963a0e6520b663
Murilo Trigo <murilotrigopaz@gmail.com>
2018-02-25 18:50:49 -0500

    Adds a primitive version of the canvas state object with drawing element selection with tab and shift-tab

--- TableTopToolKit/MainWindow.xaml.cs
+++ TableTopToolKit/MainWindow.xaml.cs
@@ -19,6 +19,34 @@ namespace TableTopToolKit
     {
         private Point lastKnownMouseDown;
         private bool mouseDown;
+        private int selectedDrawing;
+
+        private class Drawing
+        {
+            public int StartIndex { get; set; }
+            public int EndIndex { get; set; }
+
+            public Drawing(int start)
+            {
+                StartIndex = start;
+                EndIndex = -1;
+            }
+        }
+
+        private List<Drawing> drawings;
+
+        private void StartDrawing(int index)
+        {
+            drawings.Add(new Drawing(index));
+        }
+
+        private void EndDrawing(int index)
+        {
+            if (drawings.Count > 0 && drawings[drawings.Count - 1].EndIndex == -1)
+            {
+                drawings[drawings.Count - 1].EndIndex = index;
+            }
+        }
 
         private void ZoomIn()
         {
@@ -41,6 +69,8 @@ namespace TableTopToolKit
         {
             InitializeComponent();
             mouseDown = false;
+            drawings = new List<Drawing>();
+            selectedDrawing = -1;
         }
 
         private void OnCanvasMouseDown(object sender, MouseButtonEventArgs e)
@@ -56,7 +86,7 @@ namespace TableTopToolKit
                 if (mouseDown && !currentPoint.Equals(lastKnownMouseDown))
                 {
                     Line d = new Line();
-                    d.Stroke = SystemColors.WindowFrameBrush;
+                    d.Stroke = Brushes.Black;
                     d.X1 = lastKnownMouseDown.X;
                     d.Y1 = lastKnownMouseDown.Y;
                     d.X2 = currentPoint.X;
@@ -67,14 +97,65 @@ namespace TableTopToolKit
                 if (!mouseDown)
                 {
                     mouseDown = true;
+                    StartDrawing(Canvas.Children.Count);
                 }
             }
             else
             {
                 mouseDown = false;
+                EndDrawing(Canvas.Children.Count);
             }
 
             lastKnownMouseDown = currentPoint;
+            Console.Text = $"drawings: {drawings.Count}";
+        }
+
+        private void SelectDrawing(bool reverse = false)
+        {
+            if (drawings.Count > 0)
+            {
+                if (selectedDrawing >= 0)
+                {
+                    UnhightLightDrawing(drawings[selectedDrawing]);
+                }
+
+                if (reverse)
+                {
+                    selectedDrawing--;
+                    if (selectedDrawing < 0)
+                    {
+                        selectedDrawing = drawings.Count - 1;
+                    }
+                }
+                else
+                {
+                    selectedDrawing++;
+                    if (selectedDrawing >= drawings.Count)
+                    {
+                        selectedDrawing = 0;
+                    }
+                }
+
+                HightlightDrawing(drawings[selectedDrawing]);
+            }
+        }
+
+        private void UnhightLightDrawing(Drawing d)
+        {
+            for (int i = d.StartIndex; i < d.EndIndex; i++)
+            {
+                Line l = Canvas.Children[i] as Line;
+                l.Stroke = Brushes.Black;
+            }
+        }
+
+        private void HightlightDrawing(Drawing d)
+        {
+            for (int i = d.StartIndex; i < d.EndIndex; i++)
+            {
+                Line l = Canvas.Children[i] as Line;
+                l.Stroke = Brushes.Blue;
+            }
         }
 
         private void OnKeyDown(object sender, KeyEventArgs e)
@@ -91,6 +172,10 @@ namespace TableTopToolKit
 
                 case Key.Subtract:
                     ZoomOut();
+                    break;
+
+                case Key.Tab:
+                    SelectDrawing(Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift));
                     break;
             }
         }
