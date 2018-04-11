4e7176925208487072481234206760593ae62a5f
fenlys <fenlys79@gmail.com>
2018-04-11 02:02:13 -0400

    Dynamically changing color themes for everything except for icons when placed at first

--- TableTopToolKit/App.xaml.cs
+++ TableTopToolKit/App.xaml.cs
@@ -20,7 +20,8 @@ namespace TableTopToolKit
             SelectPencilTool, SelectLineTool, SelectRectangleTool, SelectEraserTool,
             Print, PrintPreview,
             AutoSave, LoadPreviousAutoSave, ToggleIconView, ToggleGridMode, SaveAs, LoadFile,
-            SelectIcon
+            SelectIcon,
+            SetStandardTheme, SetInkTheme
         };
 
         private CanvasDrawings canvasDrawings;
@@ -37,7 +38,7 @@ namespace TableTopToolKit
         public void InitializeCanvasDrawing(Canvas canvas)
         {
             canvasDrawings = new CanvasDrawings(canvas);
-            grid = new Grid(canvasDrawings.Width, canvasDrawings.Height, 64, canvasDrawings);
+            grid = new Grid(canvasDrawings.Width, canvasDrawings.Height, 64, canvasDrawings, Theme.standard.GridTheme);
             CurrentTool = new SnapLineTool(canvasDrawings, grid);
         }
 
@@ -114,6 +115,16 @@ namespace TableTopToolKit
                 case Controls.ToggleGridMode:
                     grid.ToggleMode();
                     break;
+
+                case Controls.SetStandardTheme:
+                    canvasDrawings.ChangeTheme(Theme.standard);
+                    grid.ChangeTheme(Theme.standard.GridTheme);
+                    break;
+
+                case Controls.SetInkTheme:
+                    canvasDrawings.ChangeTheme(Theme.ink);
+                    grid.ChangeTheme(Theme.ink.GridTheme);
+                    break;
             }
         }
 
@@ -128,8 +139,8 @@ namespace TableTopToolKit
             newImage.Width = iconCopy.Width;
             newImage.Height = iconCopy.Height;
 
-            ImageBrush brush = new ImageBrush(iconCopy.Source);
-            newImage.Fill = brush;
+            newImage.OpacityMask = new ImageBrush(iconCopy.Source);
+            newImage.Fill = Brushes.Black;
 
             Point snapped = grid.SnapToGridCorners(position.X - grid.SquareSize / 2, position.Y - grid.SquareSize / 2);
             Canvas.SetLeft(newImage, snapped.X);
---
+++ TableTopToolKit/Theme.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows.Media;
+
+namespace TableTopToolKit
+{
+
+    public class GridTheme
+    {
+        public Brush GridLinesColor { get; set; }
+
+        public Brush GridDotsColor { get; set; }
+
+        public int GridLinesThickness { get; set; }
+
+        public int GridDotsThickness { get; set; }
+    }
+
+
+    public class Theme {
+
+        public int LinesThickness { get; set; }
+
+        public Brush DrawingsColor { get; set; }  // lines, rectangles, pencil
+
+        public Brush IconsColor { get; set; }
+
+        public GridTheme GridTheme { get; set; }
+
+
+        public static Theme standard = new Theme() 
+        {
+            GridTheme = new GridTheme
+            {
+                GridLinesColor = Brushes.LightGray,
+                GridDotsColor = Brushes.DarkGray,
+                GridLinesThickness = 1,
+                GridDotsThickness = 3,
+            },
+            
+            LinesThickness = 3,
+            DrawingsColor = Brushes.Black,
+            IconsColor = Brushes.Black
+        };
+
+        public static Theme ink = new Theme()
+        {
+            GridTheme = new GridTheme
+            {
+                GridLinesColor = Brushes.CornflowerBlue,
+                GridDotsColor = Brushes.CornflowerBlue,
+                GridLinesThickness = 1,
+                GridDotsThickness = 3,
+            },
+
+            LinesThickness = 3,
+            DrawingsColor = Brushes.DarkBlue,
+            IconsColor = Brushes.DarkBlue
+        };
+    }
+}
